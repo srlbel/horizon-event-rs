952a5063bdd977d5942a257eb250cff2d5747945
Juan Simancas <dev.juansimancas@proton.me>
2025-03-06 08:44:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Event Horizon
+
+This is pretty much a translation for the implementation made by Tsoding in C lang.
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::f32;
+
+const WIDTH: usize = 255;
+const HEIGHT: usize = 255;
+
+fn main() {
+    let w = WIDTH as f32;
+    let h = HEIGHT as f32;
+
+    for col in 0..HEIGHT {
+        for row in 0..WIDTH {
+            let x = col as f32;
+            let y = row as f32;
+
+            let cx = (2.0 * x - w) / h;
+            let cy = (2.0 * y - h) / h;
+            let mut d = (cx * cx + cy * cy).sqrt();
+            d -= 0.5;
+            d += 0.01 * h / (2.0 * (x - y) + h - w);
+            d = d.abs();
+            d = 0.1 / d;
+
+            let chars = " -+=<@#".chars().collect::<Vec<char>>();
+            let index = ((d / (1.0 + d) * 7.0).floor() as usize).min(chars.len() - 1);
+            let c = chars[index];
+
+            print!("{}{}", c, c);
+        }
+        println!();
+    }
+}
